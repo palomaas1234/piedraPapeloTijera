98f90a0dec92faf1fc53c95f063d1dc80ab3b14d
PID <PID@DESKTOP-P7VJT1B>
2026-05-15 09:39:34 +0200

    creación del proyecto

--- piedraPapelTijera/public_html/piedraPapelTijera.js
+++ piedraPapelTijera/public_html/piedraPapelTijera.js
@@ -1,6 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
+"use_strict";
 
 
