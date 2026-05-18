d66a5aa4389501eaa01c877e052cd8bf2b15f34f
PID <PID@DESKTOP-P7VJT1B>
2026-05-18 12:46:12 +0200

    creacion del proyecto

--- piedraPapelTijera/public_html/piedraPapelTijera.js
+++ piedraPapelTijera/public_html/piedraPapelTijera.js
@@ -1,3 +1,10 @@
 "use_strict";
 
+let mensaje="Bienvenid@";
+const opciones=["piedra", "papel", "tijera"];
+let preguntaAplicacion="Que opcion prefieres 'pidrea' 'papel' 'tijera'";
+let puntuaciónJugador=" ";
+let preguntaJugador="Que opcion eliges ";
+let respueqstaJugador=" ";
+
 
