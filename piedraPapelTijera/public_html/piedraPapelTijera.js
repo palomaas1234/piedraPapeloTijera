c65539e217023cf9e39060d347dc43f0c7d701f5
PID <PID@DESKTOP-D6160RC>
2026-05-21 12:33:18 +0200

    Actualizacion de la rama Paloma poniendo los datos iniciales

--- piedraPapelTijera/public_html/piedraPapelTijera.js
+++ piedraPapelTijera/public_html/piedraPapelTijera.js
@@ -1,4 +1,11 @@
 "use_scrict";
 
+
+let mensaje="Bienvenid@";
 const opciones=["piedra", "papel", "tijeras"];
+let preguntaAplicacion="Que opcion prefieres 'pidrea' 'papel' 'tijera'";
+let puntuaciónJugador=" ";
+let preguntaJugador="Que opcion eliges ";
+let respuestaJugador=" ";
+
 
