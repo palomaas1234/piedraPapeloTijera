770fca4fae6861856941119031b7a0a89769266d
PID <PID@DESKTOP-D6160RC>
2026-05-27 11:51:21 +0200

    Actualizacion del indice de la rama Paloma sobre las diferentes opciones de respuestas que hay

--- piedraPapelTijera/public_html/piedraPapelTijera.js
+++ piedraPapelTijera/public_html/piedraPapelTijera.js
@@ -3,9 +3,35 @@
 
 let mensaje="Bienvenid@";
 const opciones=["piedra", "papel", "tijeras"];
-let preguntaAplicacion="Que opcion prefieres 'pidrea' 'papel' 'tijera'";
 let puntuaciónJugador=" ";
-let preguntaJugador="Que opcion eliges ";
+let preguntaJugador=prompt("Que opcion prefieres 'piedra' 'papel' 'tijera'");
 let respuestaJugador=" ";
+let respuestaAplicacion="";
 
+while(respuestaJugador!=respuestaAplicacion){
+    if((respuestaJugador=="piedra")&&(respuestaAplicacion=="papel")){
+     missatge="Ha ganado la aplicacion, suma 1 punto";
+ }else if((respuestaJugador=="piedra")&&(respuestaAplicacion=="tijera")){
+     missatge="Ha ganado el jugador, suma 1 punto";
+ }else if((respuestaJugador=="Papel")&&(respuestaAplicacion=="Piedra")){
+     missatge="Ha ganado el jugador, suma 1 punto";
+ }else if((respuestaJugador=="Papel")&&(respuestaAplicacion=="Tijera")){
+     missatge="Ha ganado la aplicacion, suma 1 punto";
+ }else if((respuestaJugador=="Tijera")&&(respuestaAplicacion=="Piedra")){
+     missatge="Ha ganado la aplicacion, suma 1 punto";
+ }else if((respuestaJugador=="Tijera")&&(respuestaAplicacion=="Papel")){
+     missatge="Ha ganado el jugador, suma 1 punto";
+ }
+    
+ missatge="Habeis hecho un empate";   
+    
+}
+ 
+    alert(missatge);
+ 
 
+let puntuacion=prompt("A llegado alguien a 3 puntos? 'Si', 'No'");
+
+while(puntuacion!='Si'){
+    
+}
