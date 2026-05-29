79776c39e8e248ef4f07654a75d5e590f74798cd
PID <PID@DESKTOP-P7VJT1B>
2026-05-29 09:31:01 +0200

    creación de las estructuras iterativas

--- piedraPapelTijera/public_html/piedraPapelTijera.js
+++ piedraPapelTijera/public_html/piedraPapelTijera.js
@@ -24,13 +24,11 @@ while (puntuacionJugador <3&&puntuacionAplicacion< 3) {
         (respuestaJugador=="papel"&&respuestaAplicacion=="piedra") ||
         (respuestaJugador=="tijera"&&respuestaAplicacion=="papel"))
      {
-  if (
-    respuestaJugador != "piedra" &&
-    respuestaJugador != "papel" &&
-    respuestaJugador != "tijera"
-) {
-
-    alert("Esta opción no existe");
+if(respuestaJugador!=="piedra"
+        &&respuestaJugador!=="papel"
+        &&respuestaJugador!=="tijera"){
+    alert("Esta opción no exiaste");
+}
         puntuacionJugador++;
         alert("Has ganado la ronda");
     } else {
